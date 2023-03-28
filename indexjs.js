28b4b66d1110998e12ab9a7fd625e3fa2d6ef320
Alex DUBRULLE--FAGNONI <alexdubrullefagnoni26@hotmail.fr>
2023-03-28 19:38:12 +0200

    modif addtask

--- indexjs.js
+++ indexjs.js
@@ -8,32 +8,37 @@
 
 
 // ------------- Selectors -----------------------
-let toDoList=[]
+let toDoList = []
 
-const submit= document.querySelector("#form")
+const submit = document.querySelector("#form")
 const task = document.querySelector("#nameOfItem")
 const toDoDiv = document.querySelector("#listItems")
 
 // ----------- Add task to array ----------------
 
-function addTask(){ 
-    toDoList.push(task.value)
+function addTask() {
+    //toDoList.push({
+    //  content: task.value,
+    // done: false,
+    //}
+
     task.value = ""
-    console.log (toDoList)
+    console.log(toDoList)
 }
 
 // ---------- Remove task to array ---------------
 
 function removeTask(task) {
-    toDoList.splice(task,1)
+    toDoList.splice(task, 1)
     // createTask()
     console.log(toDoList)
+    loadToDoList()
 }
 
 // ----------- Create task -----------------
 
-function createTask(task, index){
-    
+function createTask(task, index) {
+
     const newDiv = document.createElement("div")
     const newTask = document.createElement("li")
     const deleteButton = document.createElement("button")
@@ -46,40 +51,71 @@ function createTask(task, index){
     deleteButton.classList.add(".deleteButton")
     newTask.classList.add(".newLi")
 
-    
-    newTask.innerHTML = task
-    newDiv.id = index    
-    deleteButton.innerHTML= "X"
 
-    deleteButton.addEventListener("click",function (event){
+    newTask.innerHTML = task.content
+    newDiv.id = index
+    deleteButton.innerHTML = "X"
+
+
+    newTask.addEventListener("click", () => {
+        console.log("je passe à true ici")
+        let newObject = { ...toDoList[index], done: !toDoList[index].done };
+        console.log(newObject)
+        toDoList.splice(index, 1, newObject);
+
+
+        console.log(toDoList)
+
+        console.log("task.done: ", task.done)
+        if (newObject.done) {
+            console.log("je barre ici")
+            newTask.style.textDecorationLine = "line-through";
+            newTask.style.color = "red";
+        } else {
+            console.log("je supprime le trait")
+            newTask.style.color = "black";
+            newTask.style.textDecorationLine = "none";
+        }
+
+    })
+
+    deleteButton.addEventListener("click", function (event) {
         event.preventDefault()
         removeTask(index)
         loadToDoList()
     })
 
+
     // localStorage.setItem("toDoList", JSON.stringify(toDoList));
 
-    return newDiv   
+    return newDiv
 }
 
 // ----------- Upload array ----------------
 
 function loadToDoList() {
     document.querySelector("#listItems").innerHTML = "";
-    for(let i = 0; i < toDoList.length; i++) {
+    for (let i = 0; i < toDoList.length; i++) {
         document.querySelector("#listItems").appendChild(createTask(toDoList[i], i));
     }
 }
 
 // ------------ Listener ---------------
 
-submit.addEventListener("submit",function (event){
+submit.addEventListener("submit", function (event) {
     event.preventDefault()
-    if (task.value){
-        addTask(task.value)
-        console.log (toDoList)
+    if (task.value) {
+        toDoList.push({
+            content: task.value,
+            done: false
+
+        })
+        task.value = ""
+        // addTask(task.value)
+
         loadToDoList()
     }
-    
+
 })
+
 
